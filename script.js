9d699f648f398f3a8a99355121ff0e3ade503c55
gutovesco <guto.vesco@hotmail.com>
2020-01-02 00:01:20 -0300

    up

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
 const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSymbolsElement = document.getElementById('includeSymbols')
-characterAmountNumber.addEventListener('input', synccharacterAmount)
-characterAmountRangr.addEventListener('input', synccharacterAmount)
 const form = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
+
 const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
 const LOWER_CHAR_CODES = arrayFromLowtoHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
 const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
-    arrayFromLowtoHigh(58, 64).concat(
-        arrayFromLowtoHigh(91, 96).concat(
-            arrayFromLowtoHigh(123, 126)
-        )
+    arrayFromLowtoHigh(58, 64)
+    ).concat(
+    arrayFromLowtoHigh(91, 96)
+    ).concat(
+    arrayFromLowtoHigh(123, 126)
     )
-)
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRangr.addEventListener('input', syncCharacterAmount)
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -26,15 +28,14 @@ form.addEventListener('submit', e => {
     const includeSymbols = includeSymbolsElement.checked
     const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
     passwordDisplay.innerText = password
-
 })
 
-function generatePassword(characterAmount, includeUppercase, ncludeNumbers, includeSymbols){
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
     let charCodes = LOWER_CHAR_CODES
     if(includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
     if(includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
     if(includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    
+
     const passwordCharacters = []
     for(let i = 0; i < characterAmount; i++){
         const characterCode = charCodes[Math.floor(Math.random()*charCodes.lenght)]
