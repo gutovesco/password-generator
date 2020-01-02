5dd8d1815683fdc9f36ba45ba0d54da7e020f804
gutovesco <guto.vesco@hotmail.com>
2020-01-01 23:46:54 -0300

    up

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRangr.addEventListener('input', syncCharacterAmount)
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+characterAmountNumber.addEventListener('input', synccharacterAmount)
+characterAmountRangr.addEventListener('input', synccharacterAmount)
 const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const LOWER_CHAR_CODES = arrayFromLowtoHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
+    arrayFromLowtoHigh(58, 64).concat(
+        arrayFromLowtoHigh(91, 96).concat(
+            arrayFromLowtoHigh(123, 126)
+        )
+    )
+)
+
 form.addEventListener('submit', e => {
     e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
     const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+
 })
+
+function generatePassword(characterAmount, includeUppercase, ncludeNumbers, includeSymbols){
+    let charCodes = LOWER_CHAR_CODES
+    if(includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if(includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if(includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    const passwrodCharacters = []
+    for(let i = 0; i < characterAmount; i++){
+        const characterCode = charCodes[Math.floor(Math.random()*charCodes.lenght)]
+        passwrodCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwrodCharacters.join('')
+}
+
+function arrayFromLowtoHigh(low, high){
+    const array = []
+    for(let i = low; i <= high; i++){
+        arrayFromHighToLow.push(i)
+    }
+    return array
+}
 
 function syncCharacterAmount(e){
     const value = e.target.value
